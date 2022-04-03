970c73a7d5d40324d870966e291a25506f21a364
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2022-04-03 20:49:34 +0430

    optimize imports

--- src/main/java/com/accounting/config/HibernateConfig.java
+++ src/main/java/com/accounting/config/HibernateConfig.java
@@ -6,7 +6,6 @@ import org.hibernate.cfg.Environment;
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
 import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
