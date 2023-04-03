0698fb25ff93bd6d9d37b2c1bb80e35294cdb41e
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2023-04-03 20:28:20 +0330

    Add query params to filter getTrialBalanceReport

--- src/main/java/com/accounting/api/controllers/ReportController.java
+++ src/main/java/com/accounting/api/controllers/ReportController.java
@@ -3,15 +3,15 @@ package com.accounting.api.controllers;
 import com.accounting.commons.ApiResponse;
 import com.accounting.config.APIConfig;
 import com.accounting.contract.interfaces.ReportAppService;
-import com.accounting.repositories.interfaces.ReportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping(value = APIConfig.REPORT_CONTROLLER)
@@ -21,10 +21,14 @@ public class ReportController extends BaseController {
     private ReportAppService reportAppService;
 
     @GetMapping("/trial-balance")
-    public ResponseEntity<ApiResponse> getTrialBalanceReport() {
+    public ResponseEntity<ApiResponse> getTrialBalanceReport(
+            @RequestParam("accountId") long accountId,
+            @RequestParam("journalId") long journalId,
+            @RequestParam("fromDate") LocalDate fromDate,
+            @RequestParam("toDate") LocalDate toDate
+    ) {
 
-        Date date = new Date();
-        var data = reportAppService.getTrialBalanceReport(null, null, null, null);
+        var data = reportAppService.getTrialBalanceReport(accountId, journalId, fromDate, toDate);
         var resp = new ApiResponse(data, HttpStatus.OK);
         return new ResponseEntity<>(resp, HttpStatus.OK);
 
