14ed85d4f91ea07ed03a35357e0e5b72bb94883e
Reza Eskandari <44344403+RezaEskandarii@users.noreply.github.com>
2024-02-28 21:53:25 +0330

    feat: implement BalanceSheetReportAppServiceImpl class

---
+++ src/main/java/com/accounting/application/reports/BalanceSheetReportAppServiceImpl.java
@@ -0,0 +1,23 @@
+package com.accounting.application.reports;
+
+import com.accounting.contract.interfaces.reports.BalanceSheetReportAppService;
+import com.accounting.domain.interfaces.repository.BalanceSheetReportRepository;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Component
+public class BalanceSheetReportAppServiceImpl implements BalanceSheetReportAppService {
+
+    private final BalanceSheetReportRepository balanceSheetReportRepository;
+
+    public BalanceSheetReportAppServiceImpl(BalanceSheetReportRepository balanceSheetReportRepository) {
+        this.balanceSheetReportRepository = balanceSheetReportRepository;
+    }
+
+    @Override
+    public Map<String, BigDecimal> generateBalanceSheet() {
+        return balanceSheetReportRepository.generateBalanceSheet();
+    }
+}
--- src/main/java/com/accounting/contract/interfaces/reports/BalanceSheetReportAppService.java
+++ src/main/java/com/accounting/contract/interfaces/reports/BalanceSheetReportAppService.java
@@ -3,6 +3,6 @@ package com.accounting.contract.interfaces.reports;
 import java.math.BigDecimal;
 import java.util.Map;
 
-public interface BalanceSheetService {
+public interface BalanceSheetReportAppService {
     Map<String, BigDecimal> generateBalanceSheet();
 }
